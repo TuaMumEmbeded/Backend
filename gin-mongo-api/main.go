f73548f1580f25d377cebf5c783ca9efcc549988
Ploypatcharin Poolsavatkitikool <p.ploypatcharin@gmail.com>
2023-03-08 15:55:08 +0700

    modified:   gin-mongo-api/go.mod 	modified:   gin-mongo-api/go.sum 	modified:   gin-mongo-api/main.go

--- gin-mongo-api/main.go
+++ gin-mongo-api/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+        "github.com/gin-contrib/cors"
         "github.com/gin-gonic/gin"
         "gin-mongo-api/configs"
         "gin-mongo-api/routes"
 )
 
 func main() {
+        gin.SetMode(gin.ReleaseMode)
         router := gin.Default()
+
+        router.Use(cors.Default())
 
         router.GET("/", func(c *gin.Context) {
                 c.JSON(200, gin.H{
