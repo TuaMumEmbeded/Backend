05d24dd096a906131f43d68c209a46b17d623833
Sunisa Akkawong <99510218+Sunisaaaaaaaaaa@users.noreply.github.com>
2023-03-15 12:43:39 +0700

    Delete 08-03-ning directory

--- 08-03-ning/responses/user_response.go
+++
@@ -1,7 +0,0 @@
-package responses
-
-type DataResponse struct {
-	Status  int                    `json:"status"`
-	Message string                 `json:"message"`
-	Data    map[string]interface{} `json:"data"`
-}
