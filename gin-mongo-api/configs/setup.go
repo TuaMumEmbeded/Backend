d675c0a82c085fe616421632893a7fd3e1f044e9
Sunisa Akkawong <99510218+Sunisaaaaaaaaaa@users.noreply.github.com>
2023-03-15 12:49:31 +0700

    Add files via upload

--- gin-mongo-api/configs/setup.go
+++ gin-mongo-api/configs/setup.go
@@ -5,35 +5,38 @@ import (
 	"fmt"
 	"log"
 	"time"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func ConnectDB() *mongo.Client  {
+func ConnectDB() *mongo.Client {
 	client, err := mongo.NewClient(options.Client().ApplyURI(EnvMongoURI()))
 	if err != nil {
-			log.Fatal(err)
+		log.Fatal(err)
 	}
 
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
 	err = client.Connect(ctx)
 	if err != nil {
-			log.Fatal(err)
+		log.Fatal(err)
 	}
 
 	//ping the database
 	err = client.Ping(ctx, nil)
 	if err != nil {
-			log.Fatal(err)
+		log.Fatal(err)
 	}
 	fmt.Println("Connected to MongoDB")
 	return client
 }
 
-//Client instance
+// Client instance
 var DB *mongo.Client = ConnectDB()
 
-//getting database collections
+// getting database collections
 func GetCollection(client *mongo.Client, collectionName string) *mongo.Collection {
 	collection := client.Database("Bedridden").Collection(collectionName)
 	return collection
