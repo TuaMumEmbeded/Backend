eb7e1e34b739f112b4d06dbbfab31707c6d67290
Sunisa Akkawong <99510218+Sunisaaaaaaaaaa@users.noreply.github.com>
2023-03-15 12:51:34 +0700

    finish

--- gin-mongo-api/models/user_model.go
+++ gin-mongo-api/models/user_model.go
@@ -1,23 +1,14 @@
 package models
 
 import (
-	"time"
-
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type Playtime struct {
-    Play_No int `bson:"playNo,omitempty"`
-	StartTime time.Time `bson:"startTime"`
-    EndTime time.Time `bson:"endTime"`
-    Duration float64 `bson:"duration"`
-}
 type Patient struct {
-    Id       primitive.ObjectID `bson:"_id,omitempty"`
-    Patient_id    int             `bson:"patient_id,omitempty" validate:"required"`
-    Firstname     string             `bson:"firstname,omitempty" validate:"required"`
-    Lastname     string             `bson:"lastname,omitempty" validate:"required"`
-    Age     int             `bson:"age,omitempty" validate:"required"`
-    Gender     string             `bson:"gender,omitempty" validate:"required"`
-    Playtimes []Playtime `bson:"playtimes"`
+	Id         primitive.ObjectID `bson:"_id,omitempty"`
+	Patient_id int                `bson:"patient_id,omitempty" validate:"required"`
+	Firstname  string             `bson:"firstname,omitempty" validate:"required"`
+	Lastname   string             `bson:"lastname,omitempty" validate:"required"`
+	Age        int                `bson:"age,omitempty" validate:"required"`
+	Gender     string             `bson:"gender,omitempty" validate:"required"`
 }
